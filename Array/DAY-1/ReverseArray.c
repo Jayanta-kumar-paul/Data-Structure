b6e17ea441a5ad48a032f503175cc72f7420e71e
Jayanta-kumar-paul <kumarpaul912@gmail.com>
2024-05-24 23:49:18 +0530

    new

--- Array/DAY-1/ReverseArray.c
+++ Array/DAY-1/ReverseArray.c
@@ -3,7 +3,7 @@ int main ()
 {
     //1. Creation file
     int a[' '],size;
-    
+
     printf("Enter the size of array :");
     scanf("%d",&size);
     //2. Data insert
@@ -18,12 +18,20 @@ int main ()
     {
         printf("%3d",a[i]);
     }
+    //4. reverse display(backword Traversing)
+    printf("\nYour reverse Array is :");
+    for(int i=size-1;i>=0;i--)
+    {
+        printf("%3d",a[i]);
+    }
 
     return 0;
 }
-// Enter the size of array :4
+// Enter the size of array :5
+// Enter the Array data :1
 // Enter the Array data :2
 // Enter the Array data :3
 // Enter the Array data :4
 // Enter the Array data :5
-// Your Array is :  2  3  4  5
+// Your Array is :  1  2  3  4  5
+// Your reverse Array is :  5  4  3  2  1
